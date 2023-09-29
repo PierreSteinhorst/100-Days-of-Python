ae5ced8b9801f3063602f01e3d84f17961d728df
pusteinhorst <pierre-uwe.steinhorst@outlook.com>
2023-09-29 10:28:18 +0200

    add solution

---
+++ 12-day/local-global-scope.py
@@ -0,0 +1,59 @@
+######################## Scope ########################
+
+# Modify Global Scope
+# enemies = 1
+#
+#
+# def increase_enemies():
+#
+#     print(f"Enemies inside function: {enemies}")
+#     return enemies +1
+#
+# enemies = increase_enemies()
+# increase_enemies()
+# print(f"Enemies outside function: {enemies}")
+
+# Local Scope
+
+
+# def drink_potion():
+#     potion_strength = 2
+#     print(potion_strength)
+
+
+# drink_potion()
+# print(potion_strength)
+
+# Global Scope
+
+# player_health = 10
+#
+#
+# def game():
+#     def drink_potion():
+#         potion_strength = 2
+#         print(player_health)
+#     drink_potion()
+#
+#
+# print(player_health)
+
+
+# There is no block scope
+
+# game_level = 3
+# enemies = ["Skeleton", "Zombie","Alien"]
+#
+#
+# def create_enemy():
+#     if game_level < 5:
+#         new_enemy = enemies[0]
+#     print(new_enemy)
+
+# Global Constants
+
+PI = 3.14159
+URL = "https://www.google.com"
+
+
+
